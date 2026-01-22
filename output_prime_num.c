52722d43301b05dd7cc3ff02b8777de9200ef5f4
TongTONg0408 <wu204080@gmail.com>
2026-01-22 16:07:42 +0900

    输出0-100的质数

--- output_prime_num.c
+++ output_prime_num.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
-// 打印偶数
+// 打印0-100的质数
 int main(){
-    for(int i=2; i<=100; i++){
-        if (i%2==0) {
-            printf("%d ",i);
-        }else {
+    for(int n=2; n<=100; n++) {
+        int is_prime=1;
+        for (int i=2;i<=n-1;i++) {
+            if(n%i==0) {
+                is_prime=0;
+                break;
+            }
+        }
+        if(is_prime==1) {
+            printf("%d is a prime number\n",n);
         }
     }
 }
