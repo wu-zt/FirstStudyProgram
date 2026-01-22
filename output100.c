d6df13ab452f13a9b538c7dd54d29254fb15990a
TongTONg0408 <wu204080@gmail.com>
2026-01-22 15:04:42 +0900

    更改

---
+++ output100.c
@@ -0,0 +1,23 @@
+// #include <stdio.h>
+// int main() {
+//     // for (int i = 0; i <= 100; i++) {
+//     //     printf("%d\n", i);
+//     // }
+//     for (int i = 0; i <= 100; i++) {
+//         if ( i % 2 == 0) {
+//             printf("%d\n", i);
+//         }
+//         else {
+//             continue;
+//         }
+//     }
+// }
+// return,结束函数并返回
+// break,跳出当前循环
+// continue,结束本次循环，继续下次循环
+#include <stdio.h>
+int main() {
+    for (int i=0;i<=100;i++) {
+        printf("%d\n",i);
+    }
+}
